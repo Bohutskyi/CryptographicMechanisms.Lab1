8b1fb472c3d4c03a58fb32c32d08daf724330b05
Bohutskyi <oleksandr@MacBook-Pro-Oleksandr.local>
2018-04-15 13:18:41 +0300

    Rabin system

--- src/main/java/lab3/RabinClient.java
+++ src/main/java/lab3/RabinClient.java
@@ -1,76 +1,154 @@
 package lab3;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Random;
 
+/**
+ * Rabin cryptosystem.
+ *
+ * Based on using Blum numbers. It means that in our implementation program generates 2 prime numbers p = 3 (mod 4) and
+ * q = 3 (mod 4). And than n = p*q.
+ *
+ * For encrypting there is 2 restriction:
+ * 1. Message value must be less than n.
+ * 2. Message value must be greater than sqrt(n).
+ * Encryption also uses 2 int values: b1 for parity of message number, b2 for Jacobi symbol.
+ * These values also uses for decryption.
+ */
 public class RabinClient {
 
     private static final BigInteger TWO = new BigInteger("2", 10);
     private static final BigInteger THREE = new BigInteger("3", 10);
     private static final BigInteger FOUR = new BigInteger("4", 10);
     private static final BigInteger FIVE = new BigInteger("5", 10);
-    private static final BigInteger EIGTH = new BigInteger("8", 10);
+    private static final BigInteger SEVEN = new BigInteger("7", 10);
+    private static final BigInteger EIGHT = new BigInteger("8", 10);
 
     private BigInteger q, p, n;
 
+    /**
+     * Constructor in which p and q are generated.
+     * Also multiplies n = p*q.
+     *
+     * @param bitCount number of bits in q and p
+     * @param random random which is used to generation
+     */
     public RabinClient(int bitCount, Random random) {
-        q = randomPrime(bitCount - 1, random);
+        q = randomPrime(bitCount, random);
         do {
-            p = randomPrime(bitCount - 1, random);
+            p = randomPrime(bitCount, random);
         } while (q.compareTo(p) == 0);
-
-//        q = q.multiply(TWO);
-//        q = q.add(BigInteger.ONE);
-//        p = p.multiply(TWO).add(BigInteger.ONE);
-
         n = q.multiply(p);
     }
 
+    /**
+     * Getter for public key for encryption.
+     *
+     * @return n which is public key of current user
+     */
     public BigInteger getPublicKey() {
         return n;
     }
 
-    public BigInteger encrypt(BigInteger message) throws MessageException {
+    /**
+     * Encryption.
+     * Description is higher in class description.
+     *
+     * @param message BigInteger which is message for encryption
+     * @return 3 values: BigInteger cipher text and 2 int values
+     * @throws MessageException in case if restriction are disturbed
+     */
+    public Object[] encrypt(BigInteger message) throws MessageException {
         if (message.compareTo(n) == 1) {
             throw new MessageException("message is bigger than n: " + n.toString(16) + " < " + message.toString(16));
         } else if (message.compareTo(sqrt(n)) == -1 ) {
-            //WIP
-//            throw new MessageException("message is smaller than sqrt(n): " + n.toString(16) + " < " + message.toString(16));
-            throw new MessageException("WIP");
+            throw new MessageException("message is smaller than sqrt(n): " + sqrt(n).toString(16) + " > " + message.toString(16));
         }
-        return message.modPow(TWO, n);
-    }
 
-    public BigInteger[] decrypt(BigInteger cipherText) {
-        BigInteger bp1 = sqrt(cipherText);
-        bp1 = bp1.mod(p);
-        BigInteger bp2 = bp1.negate().add(p);
-        BigInteger bq1 = sqrt(cipherText);
-        bq1 = bq1.mod(q);
-        BigInteger bq2 = bq1.negate().add(q);
+        int b1, b2;
+        if (isEven(message)) {
+            b1 = 0;
+        } else {
+            b1 = 1;
+        }
 
-        System.out.println("+++++++++++++++++");
-        System.out.println(bp1.toString(10));
-        System.out.println(bp2.toString(10));
-        System.out.println(bq1.toString(10));
-        System.out.println(bq2.toString(10));
-        System.out.println("+++++++++++++++++");
-
-        BigInteger[] result = new BigInteger[4];
-
-        result[0] = (bp1.multiply(q).multiply(q.modInverse(p))).add(bq1.multiply(p).multiply(p.modInverse(q))).mod(n);
-        result[1] = (bp1.multiply(q).multiply(q.modInverse(p))).add(bq2.multiply(p).multiply(p.modInverse(q))).mod(n);
-        result[2] = (bp2.multiply(q).multiply(q.modInverse(p))).add(bq1.multiply(p).multiply(p.modInverse(q))).mod(n);
-        result[3] = (bp2.multiply(q).multiply(q.modInverse(p))).add(bq2.multiply(p).multiply(p.modInverse(q))).mod(n);
-
-//        result[1] = bp2.multiply(q).multiply(q.modInverse(p)).add(bq1).multiply(p).multiply(p.modInverse(q)).mod(n);
-//        result[2] = bp1.multiply(q).multiply(q.modInverse(p)).add(bq2).multiply(p).multiply(p.modInverse(q)).mod(n);
-//        result[3] = bp2.multiply(q).multiply(q.modInverse(p)).add(bq2).multiply(p).multiply(p.modInverse(q)).mod(n);
+        b2 = Jacobi(message, n);
+        Object[] result = new Object[3];
+        result[0] = message.modPow(TWO, n);
+        result[1] = b1;
+        result[2] = b2;
 
         return result;
     }
 
-    private static BigInteger sqrt(BigInteger n) {
+    /**
+     * Decryption.
+     *
+     * Based on using cipher text and 2 values to get 1 true message from 4 candidates.
+     *
+     * @param cipherText 3 values: BigInteger cipher text and 2 int values
+     * @return BigInteger message as decrypted cipher text
+     * */
+    public BigInteger decrypt(Object[] cipherText) {
+        BigInteger[] sqrtP = sqrtByPrimeMod((BigInteger) cipherText[0], p),
+                sqrtQ = sqrtByPrimeMod((BigInteger) cipherText[0], q);
+//        BigInteger[] result = new BigInteger[4];
+        BigInteger result;
+        int b1 = (int) cipherText[1];
+        int b2 = (int) cipherText[2];
+
+        result = (sqrtP[0].multiply(q).multiply(q.modInverse(p))).add(sqrtQ[0].multiply(p).multiply(p.modInverse(q))).mod(n);
+        result = checkCandidate(result, b1, b2);
+        if (result != null) {
+            return result;
+        }
+
+        result = (sqrtP[0].multiply(q).multiply(q.modInverse(p))).add(sqrtQ[1].multiply(p).multiply(p.modInverse(q))).mod(n);
+        result = checkCandidate(result, b1, b2);
+        if (result != null) {
+            return result;
+        }
+
+        result = (sqrtP[1].multiply(q).multiply(q.modInverse(p))).add(sqrtQ[0].multiply(p).multiply(p.modInverse(q))).mod(n);
+        result = checkCandidate(result, b1, b2);
+        if (result != null) {
+            return result;
+        }
+
+        result = (sqrtP[1].multiply(q).multiply(q.modInverse(p))).add(sqrtQ[1].multiply(p).multiply(p.modInverse(q))).mod(n);
+        result = checkCandidate(result, b1, b2);
+        if (result != null) {
+            return result;
+        }
+
+        return result;
+    }
+
+    /**
+     * Checks candidate using 2 values: parity and Jacobi symbol.
+     *
+     * @param candidate BigInteger as candidate for message
+     * @param b1 parity
+     * @param b2 Jacobi value
+     * @return candidate in case it successfully passed the test and null else
+     */
+    private BigInteger checkCandidate(BigInteger candidate, int b1, int b2) {
+        int tempB;
+        if (isEven(candidate)) {
+            tempB = 0;
+        } else {
+            tempB = 1;
+        }
+        int jacob = Jacobi(candidate, n);
+        if (tempB == b1 && jacob == b2) {
+            return candidate;
+        }
+        return null;
+    }
+
+
+    public static BigInteger sqrt(BigInteger n) {
         BigInteger a = BigInteger.ONE;
         BigInteger b = n.shiftRight(5).add(BigInteger.valueOf(8));
         while (b.compareTo(a) >= 0) {
@@ -85,66 +163,93 @@ public class RabinClient {
     }
 
     private static BigInteger[] sqrtByPrimeMod(BigInteger number, BigInteger p) {
-        if (p.mod(FOUR).compareTo(THREE) == 0) {
-            BigInteger k = p.add(BigInteger.ONE).divide(FOUR);
+        BigInteger k = p.add(BigInteger.ONE).divide(FOUR);
             BigInteger[] result = new BigInteger[2];
             result[0] = number.modPow(k ,p);
             result[1] = result[0].negate();
             return result;
-        }
-
-        if (p.mod(new BigInteger("8", 10)).compareTo(new BigInteger("5" ,10)) == 0) {
-
-        }
-
-
-        return null;
     }
 
+    /**
+     * Generates random prime BigInteger.
+     *
+     * @param bitsLength number of bits in generated number
+     * @param random random which is used to generation
+     * @return random prime BigInteger
+     */
     private static BigInteger randomPrime(int bitsLength, Random random) {
         BigInteger result;
         do {
-            result = new BigInteger(bitsLength, random);
-            result = result.multiply(TWO).add(BigInteger.ONE);
+            result = new BigInteger(bitsLength - 1, random);
+            result = result.multiply(FOUR).add(THREE);
         } while (!result.isProbablePrime(50));
         return result;
     }
 
-    public static void main(String[] args) {
-        RabinClient client = new RabinClient(7, new Random());
-//        System.out.println(client.q.toString(2));
-//        System.out.println(client.q.isProbablePrime(50));
-//        System.out.println(client.p.toString(2));
-//        System.out.println(client.p.isProbablePrime(50));
-
-        System.out.println("q = " + client.q.toString(10));
-        System.out.println(client.q.mod(new BigInteger("4", 10)).toString(10));
-        System.out.println(client.q.mod(new BigInteger("8", 10)).toString(10));
-        System.out.println("p = " + client.p.toString(10));
-        System.out.println(client.p.mod(new BigInteger("4", 10)).toString(10));
-        System.out.println(client.p.mod(new BigInteger("8", 10)).toString(10));
-
-        System.out.println("***************");
-
-        BigInteger message = new BigInteger("723", 10);
-        System.out.println("n = " + client.getPublicKey().toString(10));
-        System.out.println("m = " + message.toString(10));
-        try {
-            BigInteger c = client.encrypt(message);
-            System.out.println("cipher = " + c.toString(10));
-            System.out.println("------------");
-            for (BigInteger in : client.decrypt(c)) {
-                System.out.println(in.toString(10));
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-//        for (int i = 0; i < 25; ++i) {
-//            i1 = new BigInteger(2000, random);
-//            System.out.println(i1.toString(16));
-//            System.out.println(sqrt(i1.pow(2)).toString(16));
-//        }
+    /**
+     * Checks if specified BigInteger is even.
+     *
+     * @param number BigInteger to check
+     * @return true if number is even and false else
+     */
+    public static boolean isEven(BigInteger number) {
+        return number.getLowestSetBit() != 0;
     }
 
+    /**
+     * Calculates Jacobi symbol.
+     */
+    public static int Jacobi(BigInteger number, BigInteger n) {
+        int result = 0;
 
+        if (number.compareTo(BigInteger.ZERO) == 0)
+            result = (n.compareTo(BigInteger.ONE) == 0) ? 1 : 0;
+        else if (number.compareTo(TWO) == 0) {
+
+            BigInteger temp = n.mod(EIGHT);
+            if (temp.compareTo(BigInteger.ONE) == 0 || temp.compareTo(SEVEN) == 0) {
+                result = 1;
+            }
+
+            if (temp.compareTo(THREE) == 0 || temp.compareTo(FIVE) == 0) {
+                result = -1;
+            }
+
+        }
+        else if (number.compareTo(n) != -1)
+            result = Jacobi(number.mod(n), n);
+        else if (number.mod(TWO).compareTo(BigInteger.ZERO) == 0 )
+            result = Jacobi(TWO, n) * Jacobi(number.divide(TWO), n);
+        else
+            result = ( number.mod(FOUR).compareTo(THREE) == 0 && n.mod(FOUR).compareTo(THREE) == 0 ) ? -Jacobi(n, number) : Jacobi(n, number);
+        return result;
+    }
+
+//    public static void main(String[] args) {
+//        RabinClient client = new RabinClient(7, new SecureRandom());
+//        System.out.println(client.p.toString(2));
+//        System.out.println(client.p.isProbablePrime(50));
+//        System.out.println(client.q.toString(2));
+//        System.out.println(client.q.isProbablePrime(50));
+//        System.out.println("----------");
+//
+////
+//        BigInteger message = new BigInteger("908", 10);
+//        System.out.println("n = " + client.getPublicKey().toString(10));
+//        System.out.println("m = " + message.toString(10));
+//        try {
+//            Object[] cipher = client.encrypt(message);
+//            System.out.println("cipher = " + ((BigInteger)cipher[0]).toString(10));
+//            System.out.println("b1 = " + ((int) cipher[1]));
+//            System.out.println("b2 = " + ((int) cipher[2]));
+//            System.out.println("------------");
+//            BigInteger decr = client.decrypt(cipher);
+//            System.out.println(decr.toString(10));
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//    }
+    
 }
--- src/main/java/lab3/Test.java
+++ src/main/java/lab3/Test.java
@@ -1,24 +1,65 @@
 package lab3;
 
-import com.google.common.hash.Hashing;
-
-import java.nio.charset.StandardCharsets;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.security.SecureRandom;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
 
 public class Test {
 
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+
     public static void main(String[] args) {
-
-        String message = "some text";
-        String hash = Hashing.sha256().hashString(message, StandardCharsets.UTF_8).toString();
-        System.out.println(hash);
-        byte[] buffer = message.getBytes();
-        buffer = Sha256.getInstance().digest(buffer);
-        for (byte b : buffer) {
-            System.out.print(Integer.toHexString(b));
-        }
-
-
-
+        RabinTest();
     }
 
+    //Test for Rabin system
+    private static void RabinTest() {
+        printDate();
+        System.out.println("Test for Rabin system was started.");
+        SecureRandom random = new SecureRandom();
+        long
+                testCount = 0,
+                failures = 0;
+
+        for (int i = 100; i <= 1000; i += 100) {
+            System.out.println(i);
+            for (int j = 0; j < 5; ++j) {
+                RabinClient client = new RabinClient(i, random);
+                BigInteger message = generateMessage(client.getPublicKey(), i, random);
+                try {
+                    Object[] cipherText = client.encrypt(message);
+                    ++testCount;
+                    if (client.decrypt(cipherText).compareTo(message) != 0) {
+                        ++failures;
+                        System.out.println("Result don't match. Cipher = " + ((BigInteger)cipherText[0]).toString(16) +
+                                ", decrypt results = " + client.decrypt(cipherText).toString(16));
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        printDate();
+        DecimalFormat format = new DecimalFormat("#.####");
+        format.setRoundingMode(RoundingMode.CEILING);
+        System.out.println("Test completed:\n" + testCount + " tests, " + failures + " (" + format.format((1. * failures / testCount)) + "%) failed.");
+    }
+
+    private static BigInteger generateMessage(BigInteger n, int bitsNumber, Random random) {
+        BigInteger result;
+        do {
+            result = new BigInteger(bitsNumber + 1, random);
+        } while (result.compareTo(n) == -1 && result.compareTo(RabinClient.sqrt(n)) != 1);
+        return result;
+    }
+
+    private static void printDate() {
+        System.out.println(dateFormat.format(Calendar.getInstance().getTime()));
+    }
 }
